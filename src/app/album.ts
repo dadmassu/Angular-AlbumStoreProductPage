0652cd7827280e4fd43c97e30a26ba5842f7c5f6
Dawit Admassu <dadmassu1920@gmail.com>
2019-07-08 16:02:29 +0200

    Using intefaces to Describe Data

---
+++ src/app/album.ts
@@ -0,0 +1,9 @@
+import {Track} from 'app/track';
+
+export interface Album {
+  name: string;
+  releaseDate: string;
+  coverImage: string;
+  tracks: Track[]
+}
+
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { Album } from './album';
+
 @Injectable()
 export class ProductService {
 
@@ -13,6 +15,6 @@ export class ProductService {
 
   getAlbum(id: number) {
     return this._http.get(this._albumUrl)
-      .map(response => response.json())
+      .map(response => <Album> response.json())
   }
 }
